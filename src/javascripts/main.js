32f0b1eb200c04d32f65aabb8b241ff6a6a78ddf
Bob <bob.e.baxter@gmal.com>
2019-04-20 12:33:38 -0500

    play module works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import '../styles/main.scss';
 import eatModule from './components/eatModule';
+import playModule from './components/playModule';
 
 const init = () => {
   eatModule.eatOnLoad();
+  playModule.playOnLoad();
   eatModule.printToEat();
+  playModule.printToPlay();
 };
 
 init();
