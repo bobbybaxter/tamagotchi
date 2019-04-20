1bb07ad555d5bf9b3531b54fc56d133ca876173a
Bob <bob.e.baxter@gmal.com>
2019-04-20 12:41:05 -0500

    fight module works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,16 @@
 import '../styles/main.scss';
 import eatModule from './components/eatModule';
 import playModule from './components/playModule';
+import fightModule from './components/fightModule';
 
 const init = () => {
   eatModule.eatOnLoad();
   playModule.playOnLoad();
+  fightModule.fightOnLoad();
   eatModule.printToEat();
   playModule.printToPlay();
+  fightModule.printToFight();
+
 };
 
 init();
